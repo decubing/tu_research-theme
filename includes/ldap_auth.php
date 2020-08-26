72f88c4762bf17a984b0012812ac4ff0bca1decb
Ezra Bertuccelli <comradehiggins@gmail.com>
2020-08-26 12:18:58 -0700

    Only update roles for Students and Faculty

--- includes/ldap_auth.php
+++ includes/ldap_auth.php
@@ -8,10 +8,16 @@
 add_filter( 'authorizer_custom_role', 'trp_authorizer_set_role', 10, 2 );
 function trp_authorizer_set_role( $role, $user_data ) {
   $tu_role = $user_data['ldap_attributes'][0]['company'][0] ?? false;
+  $tu_role = strtolower($tu_role);
 
+  // Only update Student and Faculty roles
+  if ( !($tu_role === 'student' || $tu_role === 'faculty') ) {
+    return $role;
+  }
+
+  // Update roles
   $user = get_user_by( 'email', $user_data['email'] );
-  if ( $user && $tu_role ) {
-    $tu_role = strtolower($tu_role);
+  if ( $user ) {
     // update_user_meta( $user->ID, 'tu_role', $tu_role );
     $user->set_role($tu_role);
     $role = $tu_role;
