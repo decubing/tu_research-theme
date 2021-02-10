d2d2ba9d1bba8951bafbccefa27a34f25eb7a1dc
Ezra Bertuccelli <comradehiggins@gmail.com>
2021-02-10 12:26:21 -0800

    Add custom login page info

--- includes/login_logo.php
+++ includes/login_logo.php
@@ -19,4 +19,14 @@ function theme_login_logo() { ?>
   </style>
 <?php }
 add_action( 'login_enqueue_scripts', 'theme_login_logo' );
+
+function trp_login_logo_url() {
+  return home_url();
+}
+add_filter( 'login_headerurl', 'trp_login_logo_url' );
+
+function trp_login_logo_url_title() {
+  return 'Tulane Research Network';
+}
+add_filter( 'login_headertitle', 'trp_login_logo_url_title' );
 ?>
