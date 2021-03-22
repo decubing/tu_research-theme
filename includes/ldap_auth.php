26e2773922eea4049ca74f5b2a0201bfd1709207
Jon Willoth <jon@willoth.com>
2021-03-22 06:42:08 -0500

    Updated LDAP actions/filters to correctly assign role and meta data.
    Broke LDAP code in to two function, one to determine which role to
    assign and a second to update user's meta data with school.

--- includes/ldap_auth.php
+++ includes/ldap_auth.php
@@ -2,31 +2,32 @@
 // Functions specific to LDAP authorization
 
 /**
- * Update user role and meta based on LDAP company attribute
+ * Determine user role to assign
  * See https://github.com/uhm-coe/authorizer/issues/52
  */
-add_filter( 'authorizer_custom_role', 'trp_authorizer_set_role_and_meta', 10, 2 );
-function trp_authorizer_set_role_and_meta( $role, $user_data ) {
+add_filter( 'authorizer_custom_role', 'trp_authorizer_custom_role', 10, 2 );
+function trp_authorizer_custom_role( $role, $user_data ) {
   $tu_role = $user_data['ldap_attributes'][0]['company'][0] ?? false;
   $tu_role = strtolower($tu_role);
 
-  // Only update Student and Faculty roles
-  if ( !($tu_role === 'student' || $tu_role === 'faculty') ) {
-    return $role;
-  }
-
-  // Update user's role
-  $user = get_user_by( 'email', $user_data['email'] );
-  if ( $user ) {
-    $user->set_role($tu_role);
-    $role = $tu_role;
-
-    //Update user's meta
-    $school = $user_data['ldap_attributes'][0]['department'][0] ?? false;
-    if ( $school ) {
-      update_user_meta( $user->ID, 'tu_school', $school );
-    }
+  // Only approve Student and Faculty roles
+  if ( ($tu_role === 'student' || $tu_role === 'faculty') ) {
+    return $tu_role;
   }
 
   return $role;
+}
+
+/**
+ * Update user meta based on LDAP department attribute
+ */
+add_filter( 'authorizer_user_register', 'trp_authorizer_set_role_and_meta', 10, 2 );
+function trp_authorizer_set_role_and_meta( $user, $user_data ) {
+  $user_id = $user->ID;
+  
+  // If we have a department/schools to use, update user's meta 
+  $school = $user_data['ldap_attributes'][0]['department'][0] ?? false;
+  if ( $school ) {
+    update_user_meta( $user->ID, 'tu_school', $school );
+  }
 }
