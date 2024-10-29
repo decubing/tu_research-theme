624163c77a943e99986f9ae7728a121721780e8a
azdak <negatia@gmail.com>
2024-10-29 16:50:59 -0500

    Updated SoM department list dropdown to use custom query

--- includes/custom_api_endpoints.php
+++ includes/custom_api_endpoints.php
@@ -140,21 +140,35 @@ function get_som_departments()
 {
     $the_query = new WP_Query( array(
         'post_type' => 'research-listing',
+        'posts_per_page' => -1,
         'tax_query' => array(
             array (
                 'taxonomy' => 'school',
                 'field' => 'slug',
                 'terms' => 'school-of-medicine',
+                'operator' => 'IN'
             )
         ),
     ) );    
     $som_projects = $the_query -> posts;
     // NEW: make array of the post IDs in one step
     $som_projects_ids = wp_list_pluck( $som_projects, 'ID' );
+    $ids = $som_projects_ids;
+    $format = implode(', ', $ids);
     // get the terms
-    $my_terms = wp_get_object_terms( $som_projects_ids, 'department' );
+    global $wpdb;
+    $results = $wpdb->get_results( $wpdb->prepare(
+        " SELECT terms.name, terms.term_id, COUNT(tr.object_id) as count FROM {$wpdb->terms} terms " .
+        " INNER JOIN {$wpdb->term_taxonomy} tt ON (tt.term_id = terms.term_id) " .
+        " INNER JOIN {$wpdb->term_relationships} tr ON (tr.term_taxonomy_id = tt.term_taxonomy_id) " .
+        " WHERE tr.object_id IN ({$format})". 
+        " AND tt.taxonomy = 'department' ".
+        "GROUP BY terms.term_id "
+        ,
+        $som_projects_ids
+    ) );
 
-    return $my_terms;
+    return $results;
 }
 
 add_action('rest_api_init', function () {
