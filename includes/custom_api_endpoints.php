41ae82e28ea19041925e347a2f4da10deaed787b
azdak <negatia@gmail.com>
2024-10-29 16:54:57 -0500

    Updated SoM category dropdown query

--- includes/custom_api_endpoints.php
+++ includes/custom_api_endpoints.php
@@ -22,10 +22,20 @@ function get_som_cats()
     $som_projects = $the_query -> posts;
     // NEW: make array of the post IDs in one step
     $som_projects_ids = wp_list_pluck( $som_projects, 'ID' );
-    // get the terms
-    $my_terms = wp_get_object_terms( $som_projects_ids, 'category' );
-
-    return $my_terms;
+    $ids = $som_projects_ids;
+    $format = implode(', ', $ids);
+    global $wpdb;
+    $results = $wpdb->get_results( $wpdb->prepare(
+        " SELECT terms.name, terms.term_id, COUNT(tr.object_id) as count FROM {$wpdb->terms} terms " .
+        " INNER JOIN {$wpdb->term_taxonomy} tt ON (tt.term_id = terms.term_id) " .
+        " INNER JOIN {$wpdb->term_relationships} tr ON (tr.term_taxonomy_id = tt.term_taxonomy_id) " .
+        " WHERE tr.object_id IN ({$format})". 
+        " AND tt.taxonomy = 'post_cat' ".
+        "GROUP BY terms.term_id "
+        ,
+        $som_projects_ids
+    ) );
+    return $results;
 }
 
 add_action('rest_api_init', function () {
