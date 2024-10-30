1da0c5e33a60545832db5cd781d22a0592072d6e
azdak <negatia@gmail.com>
2024-10-30 13:20:55 -0500

    Fixed incorrect cat dropdown query

--- includes/custom_api_endpoints.php
+++ includes/custom_api_endpoints.php
@@ -15,28 +15,16 @@ function get_som_cats()
             array (
                 'taxonomy' => 'school',
                 'field' => 'slug',
-                'terms' => 'school-of-medicine',
-                'operator' => 'IN'
+                'terms' => 'school-of-medicine'
             )
         ),
     ) );    
     $som_projects = $the_query -> posts;
     // NEW: make array of the post IDs in one step
     $som_projects_ids = wp_list_pluck( $som_projects, 'ID' );
-    $ids = $som_projects_ids;
-    $format = implode(', ', $ids);
-    global $wpdb;
-    $results = $wpdb->get_results( $wpdb->prepare(
-        " SELECT terms.name, terms.term_id, COUNT(tr.object_id) as count FROM {$wpdb->terms} terms " .
-        " INNER JOIN {$wpdb->term_taxonomy} tt ON (tt.term_id = terms.term_id) " .
-        " INNER JOIN {$wpdb->term_relationships} tr ON (tr.term_taxonomy_id = tt.term_taxonomy_id) " .
-        " WHERE tr.object_id IN ({$format})". 
-        " AND tt.taxonomy = 'post_cat' ".
-        "GROUP BY terms.term_id "
-        ,
-        $som_projects_ids
-    ) );
-    return $results;
+    $my_terms = wp_get_object_terms( $som_projects_ids, 'category' );
+
+    return $my_terms;
 }
 
 add_action('rest_api_init', function () {
